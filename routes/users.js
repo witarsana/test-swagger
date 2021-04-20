b39aa99bdb0bc087eefb3ba3d56901dadbb71511
Witarsana <made.witarsana@gmail.com>
2021-04-20 09:25:06 +0800

    add json placholder fetch

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,28 @@ const fetch = require('node-fetch');
 const baseURL = 'https://jsonplaceholder.typicode.com/users';
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+  const url = baseURL;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      res.send({ data })
+    })
+    .catch(err => {
+      res.send(err)
+    })
 });
+
+router.get('/:id', (req, res) => {
+  const userId = req.params.id || '';
+  const url = `${baseURL}/${userId}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      res.send({ data })
+    })
+    .catch((err) => res.send(err))
+})
 
 module.exports = router;
